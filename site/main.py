55e6161689864091ca566188471b252866fc7775
Builder635 <141014389+Builder635@users.noreply.github.com>
2025-04-22 18:55:07 +0400

    main.py

--- site/main.py
+++ site/main.py
@@ -1,32 +1,71 @@
-from data.users import User
-import db_session
+
+from data.users import Users
+import sozdanie_BD
 from flask import Flask
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, logout_user, login_required
 import sqlalchemy
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField
 from wtforms.validators import DataRequired
+from flask import render_template
+from flask import redirect
+from forms.user import LoginForm, RegisterForm
 
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-
 @login_manager.user_loader
 def load_user(user_id):
-    db_sess = db_session.create_session()
-    return db_sess.query(User).get(user_id)
-
-class RegisterForm(FlaskForm):
-    email = EmailField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
-    name = StringField('Имя пользователя', validators=[DataRequired()])
-    about = TextAreaField("Немного о себе")
-    submit = SubmitField('Войти')
+    db_sess = sozdanie_BD.db_session.create_session()
+    return db_sess.query(Users).get(user_id)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('registration.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = sozdanie_BD.db_session.create_session()
+        if db_sess.query(Users).filter(Users.login == form.login.data).first():
+            return render_template('registration.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = Users()
+        user.login = form.login.data
+        user.about = form.about.data
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('registration.html', title='Регистрация', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = sozdanie_BD.db_session.create_session()
+        user = db_sess.query(Users).filter(Users.login == form.login.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+@app.route('/')
+def main():
+    return render_template('base.html', title='')
 
 
 app.run()
