ecccd877cdfb2cadbd112cabb7c19d18de39eb30
KadnikovMaksim <vomd31@gmail.com>
2025-04-27 11:08:34 +0400

    Add files via upload

--- data/questions.py
+++ data/questions.py
@@ -8,10 +8,11 @@ class Questions(SqlAlchemyBase):
     __tablename__ = 'questions'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
-                           primary_key=True, autoincrement=True)
+                           primary_key=True, autoincrement=False)
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'), nullable=False)
     questions = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     answers = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     subject = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    topic = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     created_at = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
-    user = orm.relationship("Users", back_populates="questions")
+    user = orm.relationship("Users")
