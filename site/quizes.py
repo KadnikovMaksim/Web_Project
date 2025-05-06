4b3572879e76f7a3c66449794e46b6c39e39069e
Builder635 <141014389+Builder635@users.noreply.github.com>
2025-05-06 18:50:58 +0400

    Add files via upload

---
+++ site/quizes.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, BooleanField, SubmitField, SelectField
+from wtforms.validators import DataRequired
+
+
+class CreateForm(FlaskForm):
+    topic = StringField('Тема', validators=[DataRequired()])
+    subject = SelectField('Предмет', choices=[(1, 'Математика'), (2, 'Биология'),
+                                              (3, 'Химия'), (4, 'История'), (5, 'Русский язык'),
+                                              (6, 'Физика')], validators=[DataRequired()])
+    question = StringField("Вопрос", validators=[DataRequired()])
+    answer = StringField('Ответ', validators=[DataRequired()])
+    submit = SubmitField('Создать')
+class ChangeForm(FlaskForm):
+    topic = StringField('Тема', validators=[DataRequired()])
+    subject = SelectField('Предмет', choices=[(1, 'Математика'), (2, 'Биология'),
+                                              (3, 'Химия'), (4, 'История'), (5, 'Русский язык'),
+                                              (6, 'Физика')], validators=[DataRequired()])
+    question = StringField("Вопрос", validators=[DataRequired()])
+    answer = StringField('Ответ', validators=[DataRequired()])
+    submit = SubmitField('Создать')
