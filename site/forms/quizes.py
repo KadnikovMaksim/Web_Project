e164d86c5045ab2ef6a71e13c6ddbb7cf2fb4ca4
Builder635 <141014389+Builder635@users.noreply.github.com>
2025-05-10 07:01:49 +0300

    Update quizes.py

--- site/forms/quizes.py
+++ site/forms/quizes.py
@@ -5,9 +5,7 @@ from wtforms.validators import DataRequired
 
 class CreateForm(FlaskForm):
     topic = StringField('Тема', validators=[DataRequired()])
-    subject = SelectField('Предмет', choices=[(1, 'Математика'), (2, 'Биология'),
-                                              (3, 'Химия'), (4, 'История'), (5, 'Русский язык'),
-                                              (6, 'Физика')], validators=[DataRequired()])
+    subject = StringField('Тема', validators=[DataRequired()])
     question = StringField("Вопрос", validators=[DataRequired()])
     answer = StringField('Ответ', validators=[DataRequired()])
     submit = SubmitField('Создать')
